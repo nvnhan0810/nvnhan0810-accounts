28f6f3a8d9a10c909c7fc8648ec1a76d51df594e
nvnhan <nvnhan@172-0-0-97.lightspeed.brhmal.sbcglobal.net>
2024-08-04 14:08:02 +0700

    Update welcome page

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::view('/', 'welcome');
+
 Route::get('login', [AuthController::class, 'login'])->name("login");
 Route::get('/auth/callback', [AuthController::class, 'callback'])->name("callback");
-Route::get('/auth/token', [AuthController::class, 'issueToken'])->name("token");
+
