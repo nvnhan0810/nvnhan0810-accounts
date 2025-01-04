dfb924cbd05440c9c39d560b6762a55773d53645
nguyenvannhan <nguyenvannhan0810@gmail.com>
2025-01-04 23:18:05 +0700

    Setup FE tools done

---
+++ resources/js/Pages/Login.vue
@@ -0,0 +1,3 @@
+<template>
+    <div class="text-rose-800">Login</div>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/sass/app.scss', 'resources/js/app.ts'],
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        })
+    ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler',
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+        },
+    },
+});
