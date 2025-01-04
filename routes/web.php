3c90906883fc66f22e0a38e155e40b2d3ed65e83
nguyenvannhan <nguyenvannhan0810@gmail.com>
2025-01-05 01:08:57 +0700

    Admin Login Done

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,18 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [IndexController::class, 'index']);
+Route::middleware('auth')->group(function() {
+    Route::get('/', [IndexController::class, 'index'])->name('index');
 
-Route::get('/login', [AuthController::class, 'showLoginForm']);
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+});
 
-Route::get('/oauth/login', [AuthController::class, 'login'])->name("login");
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name("login");
+Route::post('/login', [AuthController::class, 'adminLogin'])->name('admin.login.submit');
+
+Route::get('/oauth/login', [AuthController::class, 'login'])->name('client.login');
 Route::get('/auth/callback', [AuthController::class, 'callback'])->name("callback");
 
